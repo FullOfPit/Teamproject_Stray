9267803021ac29e904128dc44c607b850a103ade
thanhduong <xuanthanhduong94@gmail.com>
2023-01-08 21:06:42 +0100

    path finding algorithm

---
+++ backend/src/main/java/com/example/backend/pathfinding/Graph.java
@@ -0,0 +1,24 @@
+package com.example.backend.pathfinding;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.Map;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Graph {
+    private Map<String,Vertex> vertices;
+    private List<Edge> edges;
+    public void addVertex(String vertexName){
+        vertices.put(vertexName,new Vertex(vertexName,0,""));
+    }
+
+    public void addEdge(Edge edge){
+        edges.add(edge);
+        edges.add(new Edge(edge.getEndVertex(), edge.getStartVertex(), edge.getDistance()));
+    }
+}

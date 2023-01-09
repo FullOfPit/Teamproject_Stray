7e8202a7aff0406b7bec1c4731723ed3fdd7b0e0
thanhduong <xuanthanhduong94@gmail.com>
2023-01-09 18:10:01 +0100

    added matrix-to-graph

--- backend/src/main/java/com/example/backend/pathfinding/FindShortestPath.java
+++ backend/src/main/java/com/example/backend/pathfinding/FindShortestPath.java
@@ -18,19 +18,19 @@ public class FindShortestPath {
     private List<Vertex> unvisited;
 
 
-    public List<Vertex> findShortestPath(){
+    public List<Vertex> findShortestPath() {
         initialize();
 
-        while (!unvisited.isEmpty()){
-            Vertex evaluatedVertex= findVertexWithMinDistanceToSource();
+        while (!unvisited.isEmpty()) {
+            Vertex evaluatedVertex = findVertexWithMinDistanceToSource();
             unvisited.remove(evaluatedVertex);
             visited.add(evaluatedVertex);
 
-            Map<Vertex,Double> neighbors = findNeighborVerticesInUnvisited(evaluatedVertex);
-            for (Vertex vertex: neighbors.keySet()){
+            Map<Vertex, Double> neighbors = findNeighborVerticesInUnvisited(evaluatedVertex);
+            for (Vertex vertex : neighbors.keySet()) {
                 double distance = neighbors.get(vertex);
                 double alternativeDistanceToSource = evaluatedVertex.getDistanceToSource() + distance;
-                if (alternativeDistanceToSource < vertex.getDistanceToSource()){
+                if (alternativeDistanceToSource < vertex.getDistanceToSource()) {
                     vertex.setDistanceToSource(alternativeDistanceToSource);
                     vertex.setPreviousVertex(vertex.getName());
                 }
@@ -39,13 +39,12 @@ public class FindShortestPath {
         return visited;
     }
 
-
-    public void initialize(){
-        Map<String,Vertex> vertices=this.graph.getVertices();
-        for (Vertex vertex: vertices.values()){
-            if (vertex.getName().equals(source.getName())){
+    public void initialize() {
+        Map<String, Vertex> vertices = this.graph.getVertices();
+        for (Vertex vertex : vertices.values()) {
+            if (vertex.getName().equals(source.getName())) {
                 vertex.setDistanceToSource(0);
-            }else {
+            } else {
                 vertex.setDistanceToSource(POSITIVE_INFINITY);
             }
             vertex.setPreviousVertex("Null");
@@ -53,24 +52,26 @@ public class FindShortestPath {
         }
     }
 
-    public Vertex findVertexWithMinDistanceToSource(){
-        Vertex minVertex= this.unvisited.get(0);
-        for (Vertex vertex: this.unvisited){
-            if (vertex.getDistanceToSource() <= minVertex.getDistanceToSource()){
-                minVertex =vertex;
+    public Vertex findVertexWithMinDistanceToSource() {
+        Vertex minVertex = this.unvisited.get(0);
+        for (Vertex vertex : this.unvisited) {
+            if (vertex.getDistanceToSource() <= minVertex.getDistanceToSource()) {
+                minVertex = vertex;
             }
         }
         return minVertex;
     }
 
-    public Map<Vertex,Double> findNeighborVerticesInUnvisited(Vertex vertex){
-        Map<Vertex,Double> neighbors= new HashMap<>();
-        for (Edge edge: this.graph.getEdges()){
-            if (edge.getStartVertex().equals(vertex.getName())){
+    public Map<Vertex, Double> findNeighborVerticesInUnvisited(Vertex vertex) {
+        Map<Vertex, Double> neighbors = new HashMap<>();
+        for (Edge edge : this.graph.getEdges()) {
+            if (edge.getStartVertex().equals(vertex.getName())) {
                 Vertex endVertex = graph.getVertices().get(edge.getEndVertex());
-                neighbors.put(endVertex,edge.getDistance());
+                neighbors.put(endVertex, edge.getDistance());
             }
         }
         return neighbors;
     }
+
+
 }

fc916b260e269ca97f276aaf8518c50bf7e8a509
Claudia Hüttenrauch <claudia.huettenrauch@gmail.com>
2023-01-11 11:15:11 +0100

    added basic example for map with markers

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {MapContainer, Marker, Popup, TileLayer, Tooltip} from "react-leaflet";
+import {LatLng, LatLngExpression} from "leaflet";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // @ToDo make type?
+    const locations = [
+        {name: "Neue Fische", position: new LatLng(53.56157193989518, 9.915080727911697)},
+        {name: "Planten un Blomen", position: new LatLng(53.5625456617408, 9.98188182570993)},
+    ]
+
+    return (
+        <div className="App container">
+            <h1>Map</h1>
+            <MapContainer className="map" center={locations[0].position} zoom={13} scrollWheelZoom={false}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                {locations.map(location => (
+                    <Marker position={location.position}>
+                        <Popup>
+                            <p><strong>Popup</strong></p>
+                            <p>{location.name}</p>
+                        </Popup>
+                        <Tooltip>
+                            <p><strong>Tooltip</strong></p>
+                            <p>{location.name}</p>
+                        </Tooltip>
+                    </Marker>
+                ))}
+            </MapContainer>
+        </div>
+    );
 }
 
 export default App;

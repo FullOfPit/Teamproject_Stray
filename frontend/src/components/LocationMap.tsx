dca44e7e13d43a317ed3083307252d4e9e5519e7
thanhduong <xuanthanhduong94@gmail.com>
2023-01-20 13:14:57 +0100

    changed routing condition

--- frontend/src/components/LocationMap.tsx
+++ frontend/src/components/LocationMap.tsx
@@ -19,9 +19,9 @@ export default function LocationMap({
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <LocationMarkerGroup locations={locations}/>
+                {routing ? <Routing locations={locations}/> : <LocationMarkerGroup locations={locations}/>}
 
-                {routing ? <Routing locations={locations}/> : null}
+
             </MapContainer>
         </>
     )
